a33ae7827ca45ad294900faeb856c4303a680a86
Angela Flowers <me@angela.flowers>
2019-07-24 09:30:02 -0400

    memetastic this.js

--- assignments/this.js
+++ assignments/this.js
@@ -13,14 +13,35 @@
 
 // code example for Window Binding
 
+function squared(num) {
+//    console.log(this); // "this" points to the window object
+    return num*num;
+}
+
+console.log(squared(11));
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const user = {
+    name: "Dave",
+    email: "dbowman@nasa.gov",
+    sorry: function() {
+        console.log(`I'm sorry, ${this.name}. I'm afraid I can't do that.`);
+        console.log(this); //"this" points to the object (user) it is contained in
+        //console.log(this.name); // so this.name === user.name
+    }
+}
+user.sorry();
 
 // Principle 3
 
 // code example for New Binding
 
+
+
 // Principle 4
 
 // code example for Explicit Binding
+
